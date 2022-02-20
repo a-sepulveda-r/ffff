bc997b07544fa35db77c70f8c7dde4e0284772fb
giorn <giorn@DESKTOP-VKRJCI7>
2022-02-20 19:15:33 -0300

    Initial commit

---
+++ src/Persona.java
@@ -0,0 +1,32 @@
+
+public class Persona {
+	private String nombre ;
+	private int rut;
+	
+	public Persona(String nombre, int rut) {
+	
+		this.nombre = nombre;
+		this.rut = rut;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getRut() {
+		return rut;
+	}
+
+	public void setRut(int rut) {
+		this.rut = rut;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/principal.java
@@ -0,0 +1,16 @@
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Persona persona = new Persona("Perico",1911);
+		
+		System.out.println("la persona se llama: " + persona.getNombre() + " y su rut es: " + persona.getRut());
+			
+		
+
+	}
+
+}
